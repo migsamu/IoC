e85b75ef289c6e226fd431bdf7d6982047578395
migsamu <mufrikiler@gmail.com>
2021-10-13 13:28:26 +0200

    Spring a traves de xml(antiguo) o con anotaciones (moderno)

--- src/main/java/org/iesfm/ioc/AnotationMain.java
+++ src/main/java/org/iesfm/ioc/AnotationMain.java
@@ -1,4 +1,13 @@
 package org.iesfm.ioc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AnotationMain {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(InstitutoConfiguration.class);
+
+        InstitutoPrograma programa = context.getBean(InstitutoPrograma.class);
+        programa.ejecutar();
+    }
 }
--- src/main/java/org/iesfm/ioc/InstitutoConfiguration.java
+++ src/main/java/org/iesfm/ioc/InstitutoConfiguration.java
@@ -1,8 +1,35 @@
 package org.iesfm.ioc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class InstitutoConfiguration {
-    @Configuration
-    
+
+    @Bean
+    public InstitutoPrograma institutoPrograma(InstitutoFileScanner institutoFileScanner, InstitutoReader institutoReader, InstitutoWriter institutoWriter) {
+        return new InstitutoPrograma(institutoFileScanner, institutoReader, institutoWriter);
+    }
+
+    @Bean
+    public InstitutoWriter institutoWriter() {
+        return new InstitutoWriter();
+    }
+
+    @Bean
+    public InstitutoFileScanner fileScanner() {
+        return new InstitutoFileScanner();
+    }
+
+    @Bean
+    public InstitutoReader institutoReader(ObjectMapper objectMapper) {
+        return new InstitutoReader(objectMapper);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
 }
